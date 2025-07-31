dd2a3a9b79d6154024daed87d77afa9a11cab341
Yotam Hassin <YotamHassin@gmail.com>
2025-07-31 22:55:53 +0300

    Add files via upload

---
+++ typeof.js
@@ -0,0 +1,17 @@
+
+
+/* 
+typeof(() => {});
+'function'
+typeof({});
+'object'
+typeof(function (params) { });
+'function'
+typeof(typeof);
+VM171:1 Uncaught SyntaxError: Unexpected token ')'
+typeof(this);
+'object'
+typeof([]);
+'object'
+ */
+
