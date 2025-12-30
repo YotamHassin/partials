14fd5507b9ca0b4fcec60a482ab77663145cdec6
Yotam Hassin <YotamHassin@gmail.com>
2025-12-30 23:17:42 +0200

    Implement downloadStringify for JSON downloads
    Added downloadStringify function to download JSON data.

--- download.js
+++ download.js
@@ -7,3 +7,14 @@ function downloadCSV (data){
     var blob = new Blob([data], {type: MIME_TYPE});
     window.location.href = window.URL.createObjectURL(blob);
 }
+
+	const downloadStringify = () => {
+        const data = JSON.stringify(commands, null, 2);
+        const blob = new Blob([data], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `sketch-commands-${Date.now()}.json`;
+        a.click();
+        URL.revokeObjectURL(url);
+    };
